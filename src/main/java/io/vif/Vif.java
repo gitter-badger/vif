b7c24fbf5e121666e6c161032c094134d33ecf25
kaanyagci <kaan.yagci@clique.paris>
2020-12-28 15:37:43 +0100

    Project renamed to Vif

--- src/main/java/io/vif/Vif.java
+++ src/main/java/io/vif/Vif.java
@@ -1,10 +1,10 @@
-package io.octal;
+package io.vif;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
-import io.octal.proto.File;
-import io.octal.proto.ProjectFiles;
+import io.vif.proto.File;
+import io.vif.proto.ProjectFiles;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -16,8 +16,8 @@ import java.util.logging.Logger;
 /**
  * Server that manages startup/shutdown of a {@code Greeter} server.
  */
-public class Octal {
-    private static final Logger logger = Logger.getLogger(Octal.class.getName());
+public class Vif {
+    private static final Logger logger = Logger.getLogger(Vif.class.getName());
 
     private Server server;
 
@@ -36,7 +36,7 @@ public class Octal {
                 // Use stderr here since the logger may have been reset by its JVM shutdown hook.
                 System.err.println("*** shutting down gRPC server since JVM is shutting down");
                 try {
-                    Octal.this.stop();
+                    Vif.this.stop();
                 } catch (InterruptedException e) {
                     e.printStackTrace(System.err);
                 }
@@ -64,21 +64,21 @@ public class Octal {
      * Main launches the server from the command line.
      */
     public static void main(String[] args) throws IOException, InterruptedException {
-        final Octal server = new Octal();
+        final Vif server = new Vif();
         server.start();
         server.blockUntilShutdown();
     }
 
-    static class OctalImpl extends io.octal.proto.OctalGrpc.OctalImplBase {
+    static class OctalImpl extends io.vif.proto.OctalGrpc.OctalImplBase {
 
         @Override
-        public void sendFile(io.octal.proto.ProjectFiles req, StreamObserver<io.octal.proto.ProjectFiles> responseObserver) {
+        public void sendFile(io.vif.proto.ProjectFiles req, StreamObserver<io.vif.proto.ProjectFiles> responseObserver) {
             System.out.println("Send file function called");
             System.out.println("Recieved " + req.getFilesList().size() + " files");
              List<File> inputFilesList = req.getFilesList();
 
             //TODO: Get files from request and send to appropriate compiler
-            io.octal.proto.ProjectFiles.Builder fb = io.octal.proto.ProjectFiles.newBuilder();
+            io.vif.proto.ProjectFiles.Builder fb = io.vif.proto.ProjectFiles.newBuilder();
             fb.addAllFiles(new ArrayList<File>());
             fb.setEpoch(Instant.now().toEpochMilli());
             ProjectFiles reply = fb.build();
